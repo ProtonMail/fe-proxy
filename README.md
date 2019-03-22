f671464b6eaa3dcc5f751eff84d4b8fad6eeb5c3
mmso <mmso@mmso.se>
2019-03-22 15:18:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# fe-proxy
+
+A simple proxy server that redirects to different urls.
+
+## Options
+
+```bash
+--route='regex url'
+--port=number
+```
+
+where `--route` is split on whitespace, and creates a regex of the first string and a url of the second string. If only a url is specified, it will match anything.
+
+## Example
+
+```bash
+node index \
+    --route='^/settings/ http://localhost:8081' \
+    --route='http://localhost:8080' \
+    --port=8040
+```
+
+Would setup a server at port `8040` that redirects to `localhost:8081/settings/` when the url matches the regex `^/settings/` and redirect to `http://localhost:8080` otherwise.
+
+| URL | Redirect |
+| --- | --- |
+|`localhost:8040/settings/` | `localhost:8081/settings/` |
+|`localhost:8040/settings/abc` | `localhost:8081/settings/abc` |
+|`localhost:8040/` | `localhost:8080/` |
+|`localhost:8040/abc` | `localhost:8080/abc` |
+|`localhost:8040/abc/settings` | `localhost:8080/abc/settings` |
---
+++ index.js
@@ -0,0 +1,74 @@
+const http = require('http');
+const argv = require('minimist')(process.argv.slice(2));
+
+const port = argv.port || 8040;
+
+const createResolver = (routes = []) => {
+    if (!Array.isArray(routes) || routes.length <= 0) {
+        throw new Error('Nothing to route');
+    }
+
+    const paths = routes.map((route) => {
+        const split = route.split(' ');
+        if (split.length > 2) {
+            throw new Error('Invalid route specified');
+        }
+        if (split.length === 1) {
+            split.unshift('')
+        }
+        const [regexString, urlString] = split;
+        return [new RegExp(regexString), new URL(urlString)];
+    });
+
+    console.log('Routes:\n');
+    console.log(paths.map(path => path.join(' -> ')).join('\n'));
+    console.log('');
+
+    return (url) => {
+        const result = paths.find(([regex, toUrl]) => {
+            if (regex.test(url)) {
+                return toUrl;
+            }
+        });
+        if (!result) {
+            throw new Error('No match found');
+        }
+        const [, toUrl] = result;
+        return toUrl;
+    }
+};
+
+const resolver = createResolver(argv.route);
+
+const server = http.createServer((req, res) => {
+    const { method, url, headers } = req;
+    const toUrl = resolver(url);
+    console.log(`[${method}] ${url} -> [${method}] ${toUrl.hostname}:${toUrl.port}${url}`);
+
+    const proxyOptions = {
+        port: toUrl.port,
+        hostname: toUrl.hostname,
+        path: url,
+        headers,
+        method
+    };
+
+    const proxyCallback = (proxyResult) => {
+        res.writeHead(proxyResult.statusCode, proxyResult.headers);
+        proxyResult.pipe(res);
+    };
+
+    const proxyReq = http.request(proxyOptions, proxyCallback);
+
+    proxyReq.on('error', (e) => {
+        console.error('Proxy request fail: ' + e.message, e);
+        res.writeHead(500);
+        res.end();
+    });
+
+    req.pipe(proxyReq)
+});
+
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
